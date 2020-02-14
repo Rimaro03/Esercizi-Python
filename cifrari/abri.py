8fa5a9432c7f2382ede5c8b712785be06fd1ffea
Rimaro03 <57857328+Rimaro03@users.noreply.github.com>
2020-02-14 15:44:08 +0100

    Add files via upload

---
+++ cifrari/abri.py
@@ -0,0 +1,59 @@
+p=input("Inserisci la parola: ")
+decod=""
+i=0
+while i<len(p):
+  if p[i]=="c":
+    decod=decod+"a"
+  elif p[i]=="a":
+    decod=decod+"b"
+  elif p[i]=="r":
+    decod=decod+"c"
+  elif p[i]=="e":
+    decod=decod+"d"
+  elif p[i]=="f":
+    decod=decod+"e"
+  elif p[i]=="u":
+    decod=decod+"f"
+  elif p[i]=="l":
+    decod=decod+"g"
+  elif p[i]=="b":
+    decod=decod+"h"
+  elif p[i]=="d":
+    decod=decod+"i"
+  elif p[i]=="g":
+    decod=decod+"j"
+  elif p[i]=="h":
+    decod=decod+"k"
+  elif p[i]=="i":
+    decod=decod+"l"
+  elif p[i]=="j":
+    decod=decod+"m"
+  elif p[i]=="k":
+    decod=decod+"n"
+  elif p[i]=="m":
+    decod=decod+"o"
+  elif p[i]=="n":
+    decod=decod+"p"
+  elif p[i]=="o":
+    decod=decod+"q
+  elif p[i]=="p":
+    decod=decod+"r"
+  elif p[i]=="q":
+    decod=decod+"s"
+  elif p[i]=="s":
+    decod=decod+"t"
+  elif p[i]=="t":
+    decod=decod+"u"
+  elif p[i]=="v":
+    decod=decod+"v"
+  elif p[i]=="w":
+    decod=decod+"w"
+  elif p[i]=="x":
+    decod=decod+"x"
+  elif p[i]=="y ":
+    decod=decod+"y"
+  elif p[]
+  else:
+    decod=decod+" "
+  i=i+1
+print(decod)
